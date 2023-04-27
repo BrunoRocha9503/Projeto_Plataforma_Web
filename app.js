51be889f51a10c4c7480aebe6668d0369af80ab1
Kelvin Willian <kelvin.faccini@gmail.com>
2023-04-27 18:07:24 -0300

    Implementação authenticação
    Realizada a authenticação para login.

--- app.js
+++ app.js
@@ -4,15 +4,58 @@ const app = express();
 const routes = require('./routes');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'mySecret', resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+const { readData, writeData } = require('./auth/filestorage');
+const users = readData();
+
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      const user = users.find((user) => user.email === email);
+      if (!user) {
+        return done(null, false, { message: 'Usuário não encontrado' });
+      }
+  
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Senha incorreta' });
+        }
+      });
+    })
+  );
+  
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser((id, done) => {
+    const user = users.find((user) => user.id === id);
+    done(null, user);
+  });
+
+  app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado!');
+  });
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,71 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const { readData, writeData } = require('../auth/filestorage');
+
+const users = readData();
 
 router.get('/', (req, res) => {
-  res.render('login', {title: 'Login'});
+  res.render('login',{ message: req.flash('error'), success: req.flash('success')[0] });
 });
 
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/index',
+    failureRedirect: '/',
+    failureFlash: true,
+  })
+);
+
 router.get('/index', (req, res) => {
-  res.render('index', {title: 'Pagina inicial'});
+    if (req.isAuthenticated()) {
+      res.render('index');
+    } else {
+      res.redirect('/');
+    }
 });
 
 router.get('/perfil', (req, res) => {
-  res.render('perfil', {title: 'Perfil'});
+  if (req.isAuthenticated()) {
+    res.render('perfil');
+  } else {
+    res.redirect('/');
+  }
 });
 
 router.get('/cadastro', (req, res) => {
-  res.render('cadastro', {title: 'Cadastro'});
+  res.render('cadastro', { message: req.flash('error') });
+});
+
+router.post('/cadastro', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user = {
+      id: Date.now(),
+      nome: req.body.nome,
+      email: req.body.email,
+      data: req.body.data,
+      password: hashedPassword
+    };
+    users.push(user);
+    writeData(users);
+    res.redirect('/');
+  } catch (err) {
+    req.flash('error', 'Ocorreu um erro ao tentar criar a conta');
+    res.redirect('/cadastro');
+  }
+});
+
+router.get('/logout', (req, res) => {
+  const successMessage = 'Você saiu da sua conta';
+  req.session.destroy(err => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/index');
+    }
+    res.render('login', { message: null, success: successMessage });
+  });
 });
 
 module.exports = router;
+
